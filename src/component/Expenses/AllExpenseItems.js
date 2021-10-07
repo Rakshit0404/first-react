2509ee7be0a893d8a1256702f5e9f37fc37419ec
Rakshit0404 <saritasakshi52@gmail.com>
2021-10-08 00:09:36 +0530

    hello

--- src/component/Expenses/AllExpenseItems.js
+++ src/component/Expenses/AllExpenseItems.js
@@ -5,17 +5,28 @@ import ExpensesFilter from './ExpensesFilter';
 import { useState } from 'react';
 
 function AllExpenseItems(props) {
-    const expenses = props.data;
+    const expenses=props.data;
     const [filterYear, setFilterYear] = useState('2020');
     const filterChangeHandler = (selectedInterval) => {
         setFilterYear(selectedInterval);
     }
 
+    const expensesFilter=expenses.filter((expense)=>{
+        return expense.date.getFullYear()==filterYear;
+    });
+
+    let component=<p>No expenses in this Year</p>
+
+    if(expensesFilter.length>0)
+    {
+        component=expensesFilter.map(exp=>(<ExpenseItem key={exp.id} title={exp.title} amount={exp.amount} date={exp.date}></ExpenseItem>))
+    }
+    
     return (
         <div className="expenses-filter">
             <Card className="expenses">
                 <ExpensesFilter selected={filterYear} onChangeFilter={filterChangeHandler}></ExpensesFilter>
-                {expenses.map(exp=>(<ExpenseItem key={exp.id} title={exp.title} amount={exp.amount} date={exp.date}></ExpenseItem>))}
+                {component}
             </Card>
         </div>
     );
