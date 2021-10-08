741ec750d873c94b13065b2ab5e2280308011abd
Rakshit0404 <saritasakshi52@gmail.com>
2021-10-08 19:46:48 +0530

    hiii

--- src/component/Expenses/ExpenseItem.js
+++ src/component/Expenses/ExpenseItem.js
@@ -1,17 +1,19 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 import Card from '../UI/Card.js';
 
 function ExpenseItem(props) {
     return (
-        <Card className="expense-item">
-        <ExpenseDate date={props.date}></ExpenseDate>
-            <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">Rs {props.amount}</div>
-            </div>
-        </Card>
+        <li>
+            <Card className="expense-item">
+                <ExpenseDate date={props.date}></ExpenseDate>
+                <div className="expense-item__description">
+                    <h2>{props.title}</h2>
+                    <div className="expense-item__price">Rs {props.amount}</div>
+                </div>
+            </Card>
+        </li>
     );
 }
 
